9843fad35f3f13b18521f6922839abd3ee081af0
Valentin Kusto <valya.kusto@gmail.com>
2019-06-01 16:34:16 -0400

    Added Login API Method

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const express = require('express');
-const app = express();
+const express = require('express'),
+    app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World ' + new Date());
+
+app.use(express.json());
+
+app.post('/login', function(req, res) {
+    var userName = req.body.user_name;
+    var pw = req.body.pw;
+
+    if(userName === 'test' && pw === '1234') {
+        res.json({token: Date.now()});
+
+    }
+    else {
+        res.send(404, 'USER_NOT_FOUND');
+    }
 });
 
 app.get('/timeStamp', function (req, res) {
